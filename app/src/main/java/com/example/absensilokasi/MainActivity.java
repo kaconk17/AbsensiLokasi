c98201869773b29203aeff929b728fa782bcf0c2
kaconk <harutonouciha@gmail.com>
2020-05-18 21:13:05 +0700

    popup menu logout

--- app/src/main/java/com/example/absensilokasi/MainActivity.java
+++ app/src/main/java/com/example/absensilokasi/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.absensilokasi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 import java.util.HashMap;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
     SessionManager session;
+    CircleImageView imgProfile;
+    AlertDialogManager alert = new AlertDialogManager();
 Boolean login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,34 @@ Boolean login;
         String name = user.get(SessionManager.KEY_NAME);
         String email = user.get(SessionManager.KEY_EMAIL);
 
+        imgProfile = findViewById(R.id.profile_pic);
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, imgProfile);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.popup_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.logout:
+                                alert.showAlertDialog(MainActivity.this, "Logout !", "Anda logout", false);
+                                return true;
+                            case R.id.one:
+                                alert.showAlertDialog(MainActivity.this, "One !", "Anda logout", true);
+                                return true;
+                            default:
+                                return false;
+                        }
+
+                    }
+                });
+                popup.show();
+
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/example/absensilokasi/loginActivity.java
+++ app/src/main/java/com/example/absensilokasi/loginActivity.java
@@ -2,13 +2,48 @@ package com.example.absensilokasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginActivity extends AppCompatActivity {
+    EditText txtemail, txtpass;
+    Button btn_login;
+    AlertDialogManager alert = new AlertDialogManager();
+    SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        session = new SessionManager(getApplicationContext());
+        txtemail = findViewById(R.id.input_email);
+        txtpass = findViewById(R.id.input_password);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtemail.getText().toString();
+                String pass = txtpass.getText().toString();
+
+                if (email.trim().length() > 0 && pass.trim().length() > 0){
+                    if (email.equals("admin@gmail.com") && pass.equals("admin")){
+                        session.createLoginSession("Admin", "admin@gmail.com");
+
+                        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        alert.showAlertDialog(loginActivity.this, "Login Gagal !", "Email / Password Salah", false);
+                    }
+                }else{
+                    alert.showAlertDialog(loginActivity.this, "Login Gagal !", "Mohon Masukkan Email dan Password", false);
+                }
+            }
+        });
     }
 }
